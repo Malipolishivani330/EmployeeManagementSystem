dc8df74180f824840d00f77e30ef1601735d7a8b
Malipolishivani330 <malipolishivani3027@gmail.com>
2026-06-11 13:43:19 +0530

    Allow public GET requests

--- employees/views.py
+++ employees/views.py
@@ -13,9 +13,9 @@ def home(request):
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            return request.user and request.user.is_authenticated
-        return request.user and request.user.is_staff
+            return True
 
+        return request.user and request.user.is_staff
 
 # List and Create Employees
 class EmployeeListCreateView(generics.ListCreateAPIView):
